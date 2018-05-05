a3cbb5f923bd7a63b2b560975075b1fef7f6b819
Rassul Yunussov <yunussov@gmail.com>
2018-05-05 12:13:23 +0600

    stats for bp in dynamics

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -17,6 +17,7 @@ namespace counter.Controllers
         {
             
         }
+        
         private bool InPeriod(string period, DateTime date)
         {
             bool result = false;
@@ -35,7 +36,7 @@ namespace counter.Controllers
             return result;
         }
         [HttpGet("{period?}")]
-        public async Task<IActionResult> BusinessPointsStats(string period)
+        public async Task<IActionResult> BusinessPoints(string period)
         {
            var user = await _userManager.GetUserAsync(User);
            string ownerId = user.Id;
@@ -45,6 +46,29 @@ namespace counter.Controllers
                         select new {bp.Id,t.Amount}).GroupBy(b=>b.Id)
                                                     .Select(b => new { businessPointId = b.Key, amount = b.Sum(t=>t.Amount)});
            return Json(res);
+        }
+        [HttpGet("{businessPointId}/{startDate}/{endDate}/{period?}")]
+        public async Task<IActionResult> BusinessPointDynamics(int businessPointId, string period, DateTime startDate, DateTime endDate)
+        {
+           var user = await _userManager.GetUserAsync(User);
+           string ownerId = user.Id;
+           var res = (from bp in _ctx.BusinessPoints
+                        join t in _ctx.Tickets on bp.Id equals t.BusinessPoint.Id
+                        where bp.Id==businessPointId 
+                                && bp.Owner.Id==ownerId 
+                                && t.OperationDate >= startDate
+                                && t.OperationDate < endDate
+                        select t);
+          
+           switch(period) 
+           {
+               case "y":
+                        return Json(res.GroupBy(t=>t.OperationDate.Year).Select(g=>new { period = g.Key, total = g.Sum(gg=>gg.Amount)}));
+               case "m":
+                        return Json(res.GroupBy(t=>t.OperationDate.ToString("yyyy-MM")).Select(g=>new { period = g.Key, total = g.Sum(gg=>gg.Amount)}));
+               default:
+                        return Json(res.GroupBy(t=>t.OperationDate.ToShortDateString()).Select(g=>new { period = g.Key, total = g.Sum(gg=>gg.Amount)}));
+           }
         }
     }
 }
